7a9f4c96d6ca0159f322cfff6501480e878e56b0
wiel8526 <wiel8526@colorado.edu>
2015-04-28 00:49:12 -0600

    Update and rename final.h to Person.h
    Adding minor changes and removing the queue

---
+++ Person.h
@@ -0,0 +1,32 @@
+#ifndef PERSON_H
+#define PERSON_H
+#include <string>
+
+struct node{
+    std::string full_name;
+	std::string nickname;
+	std::string number;
+	std::string address;
+    node *next;
+    node *previous;
+};
+
+class Person
+{
+	private:
+		node *head;
+        node *tail;
+		node* searchList(node * node, std::string Nickname);
+	public:
+		void addContact(std::string, std::string, std::string, std::string);
+		void deleteContact(std::string NicName);
+		void findContact(std::string NicName);
+		void displayAllContacts();
+		void changeNumber(std::string NicName, std::string newNumber);
+		void changeAddress(std::string NicName, std::string newAddress);
+		
+		Person(std::string Fname, std::string NicName, std::string Number, std::string Address);
+		virtual ~Person();
+};
+	
+#endif
